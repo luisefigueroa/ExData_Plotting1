0b9e1492529ce9bb82cfc2e60e031c934cb1be01
luisefigueroa <luisfigueroa@Luiss-MacBook-Pro.local>
2015-03-08 09:51:25 -0400

    Commented scripts

--- Plot1.R
+++ Plot1.R
@@ -1,9 +1,22 @@
+##############################################################################################
+# Script written by: Luis Figueroa
+#
+# This script was produced for the first assignment of the reproducible research course of the  
+# Johns Hopkins University's Data Science specialization track in Coursera
+#
+##############################################################################################
+
+########## Download the raw data ##########
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 destination <- "./r/downloads/PowerConsumption.zip"
 download.file(url, destination, method = "curl")
+
+########## Read the raw data into memory and subset the data set to include only data for 2007-02-01 and 2007-02-02 ##########
 PC <- read.table(unz(destination, filename="household_power_consumption.txt"), header = TRUE, sep = ";", na.strings = "?")
 PC$Date = as.Date(as.character(PC$Date), format = "%d/%m/%Y")
 PC <- subset(PC, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+######### Creates plot ##########
 png(file = "./R/ExData_Plotting1/Plot1.png", width = 480, height = 480)
 hist(PC$Global_active_power, freq = TRUE, breaks = 12 , main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
 dev.off()
--- Plot3.R
+++ Plot3.R
@@ -1,10 +1,23 @@
+##############################################################################################
+# Script written by: Luis Figueroa
+#
+# This script was produced for the first assignment of the reproducible research course of the  
+# Johns Hopkins University's Data Science specialization track in Coursera
+#
+##############################################################################################
+
+########## Download the raw data ##########
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 destination <- "./r/downloads/PowerConsumption.zip"
 download.file(url, destination, method = "curl")
+
+########## Read the raw data into memory and subset the data set to include only data for 2007-02-01 and 2007-02-02 ##########
 PC <- read.table(unz(destination, filename="household_power_consumption.txt"), header = TRUE, sep = ";", na.strings = "?")
 PC$DateTime = paste(PC$Date, PC$Time, sep = " ")
 PC$DateTime = strptime(PC$DateTime, format = "%d/%m/%Y %H:%M:%S")
 PC <- subset(PC, DateTime >= "2007-02-01" & DateTime < "2007-02-03")
+
+######### Creates plot ##########
 png(file = "./R/ExData_Plotting1/Plot3.png", width = 480, height = 480)
 plot(PC$DateTime, PC$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
 lines(PC$DateTime, PC$Sub_metering_2, type = "l", col = "red")
